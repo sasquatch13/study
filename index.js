3a2e49ce081b4acb9883f16227d42d265d6b2fc2
sasquatch13 <115385918+sasquatch13@users.noreply.github.com>
2023-10-16 00:39:17 -0400

    added button

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 import {termData} from "./data.js"
-console.log(termData)
 
+const defList = document.getElementById("def-list")
 const card = document.getElementById('card')
+const btn = document.getElementById("btn")
 let clicked = false
+const list = []
+let prevNum
 
 function getRandomNumber(){
     return Math.floor((Math.random() * termData.length))}
 
 let randomNumber = getRandomNumber()
 
-console.log(termData.length)
-
-
-console.log(randomNumber)
-
 card.innerHTML = '<h2 class="hand front">Click Me!</h2>'
 
 card.addEventListener("click", ()=> {clicked = !clicked
     if (clicked) {
+       btn.disabled = true 
+        if (clicked = true){
+            btn.disabled = false
+        }
         card.innerHTML = `<h2 class="hand front">${termData[randomNumber].term}</h2>`
-    } else {card.innerHTML = `<h2 class="hand back">${termData[randomNumber].definition}</h2>`
+    } else {
+        btn.disabled = false
+        card.innerHTML = `<h2 class="hand back">${termData[randomNumber].definition}</h2>`
+    prevNum = randomNumber
     randomNumber = getRandomNumber()}
-    console.log(clicked)})
+    })
 
+btn.addEventListener("click", function(){
+    defList.innerHTML += `<li>${termData[prevNum].term}${termData[prevNum].definition}</li>`
+})
